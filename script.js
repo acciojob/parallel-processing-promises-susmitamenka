0d45e7dec965c633eece945b9b0eb2339096b227
susmitamenka <susmitaprasad73@gmail.com>
2025-03-01 12:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImage(link){
+	return new promise((resolve,reject)=>{
+		let random = parseInt(Math.random()*10000)
+		if(link.startWith("https")){
+			setTimeout(()=>{
+				resolve({
+					image: link,
+					message: "image downloaded sucessfully"
+				})
+			},3000)
+		}
+		else{
+			setTimeout(()=>{
+				reject(`image could not be downloaded ${link}`)
+			},3000)
+		}
+	
+		})
+}
+let arr = []
+for(let img of images){
+	arr.push(downloadImage(img.url))
+}
+promise.all(arr).then(
+	(res)=>{
+		console.log(res)
+		for(let t of res){
+			let img = document.createElement("img")
+			img.src = t.image
+			output.append(img)
+		}
+	}
+)
+.catch(
+	(err)=>{
+	console.log(err)
+}
+)
